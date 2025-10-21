66eadbc3e2f549d259a610a1524ef0c1c4d6003b
xan-d <145882987+xan-d@users.noreply.github.com>
2025-10-21 01:16:04 -0700

    testing version counter and git hook

--- Unity/Kartographer/increment_version.bat
+++ Unity/Kartographer/increment_version.bat
@@ -1,11 +1,16 @@
 @echo off
-REM No quotes here
-SET UNITY_EXE=C:\Program Files\Unity\Hub\Editor\6000.2.6f2\Editor\Unity.exe
+REM Move up from Unity/Kartographer to the Unity project root
+pushd "%~dp0"
+
+REM Go back to Unity project folder (the current folder)
 SET PROJECT_PATH=%CD%
 
-REM Quote it when calling
+REM Path to Unity executable (relative or from PATH)
+SET UNITY_EXE=C:\Program Files\Unity\Hub\Editor\6000.2.6f2\Editor\Unity.exe
+
+REM Run Unity batch mode
 "%UNITY_EXE%" -batchmode -quit -projectPath "%PROJECT_PATH%" -executeMethod VersionIncrementer.IncrementBuildNumber -logFile -
 
-echo Unity finished running.
-pause
-
+echo Version increment process finished.
+popd
+exit /b 0
